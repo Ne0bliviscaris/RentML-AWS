e5752605be1f71e8c743cd6254d3cf2054ec822a
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-07-13 16:28:15 +0200

    fix: preparing image for OCR

--- sagemaker-ocr/ocr.py
+++ sagemaker-ocr/ocr.py
@@ -1,12 +1,17 @@
+import io
 import re
 
 import easyocr
+import numpy as np
+from PIL import Image
 
 
 def mileage_ocr(image_bytes):
     """Extract mileage from image using easyocr."""
-    ocr = easyocr.Reader(["en"])
-    ocr_result = ocr.readtext(image_bytes, allowlist="0123456789")
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    image_np = np.array(image)
+    reader = easyocr.Reader(["en"])
+    ocr_result = reader.readtext(image_np, allowlist="0123456789")
     SIX_DIGITS = r"\b\d{6}\b"
     six_digit_numbers = re.findall(SIX_DIGITS, str(ocr_result))
     if six_digit_numbers:
