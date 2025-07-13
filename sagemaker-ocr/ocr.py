f9ad8d52a30d0cb17b63fed8aad17f95d6c84841
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-07-13 16:13:19 +0200

    feature: refactor mileage OCR function and add requirements file

--- sagemaker-ocr/ocr.py
+++ sagemaker-ocr/ocr.py
@@ -1,11 +1,14 @@
+import re
+
 import easyocr
 
 
-def mileage_ocr(image):
+def mileage_ocr(image_bytes):
     """Extract mileage from image using easyocr."""
-    reader = easyocr.Reader(["en"])
-    result = reader.readtext(image)
-    for _, text, _ in result:
-        if text.isdigit():
-            return {"mileage": text}
+    ocr = easyocr.Reader(["en"])
+    ocr_result = ocr.readtext(image_bytes, allowlist="0123456789")
+    SIX_DIGITS = r"\b\d{6}\b"
+    six_digit_numbers = re.findall(SIX_DIGITS, str(ocr_result))
+    if six_digit_numbers:
+        return {"mileage": six_digit_numbers[0]}
     return {"mileage": "Not found"}
